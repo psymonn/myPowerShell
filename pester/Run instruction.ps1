3d31d40a2d37229a58fb38c68a48ce3822797d78
psymonn <40018954+psymonn@users.noreply.github.com>
2018-08-07 00:52:42 +1000

    Run instruction

---
+++ pester/Run instruction.ps1
@@ -0,0 +1,4 @@
+﻿Install-Module Pester -RequireVersion 4.1.0 -SkipPublisherCheck
+get-module pester -ListAvailable
+Invoke-Pester -script .\Get-ServiceProperty.Tests.ps1
+PS C:\DebuggingPowerShellVSCode\M5> Invoke-Pester  #this run all test that suffix with .Tests.ps1
