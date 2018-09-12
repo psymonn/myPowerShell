d3df3d90f0762d1c6de40123681c3aa6dcaf4782
psymonn <40018954+psymonn@users.noreply.github.com>
2018-09-12 11:47:04 +1000

    Update test4.ps1

--- Scope/ModuleFunction/Solution Set Global preference scope/test4.ps1
+++ Scope/ModuleFunction/Solution Set Global preference scope/test4.ps1
@@ -1,3 +1,9 @@
+<#
+By default, this will retrieve the values of all PowerShell Preference variables
+(as defined in the about_Preference_Variables help file) and set them in your function's local scope, 
+unless the variable has an associated Common Parameter (-Verbose, etc) which has been set in the $PSBoundParameters table.
+#>
+
 function Test-ModuleFunction
 {
     [CmdletBinding()]
