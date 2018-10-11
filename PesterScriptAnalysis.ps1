6e19e16646952e3633f94256a8ce0837a22a8372
psymonn <40018954+psymonn@users.noreply.github.com>
2018-10-11 16:09:49 +1100

    Create PesterScriptAnalysis.ps1

---
+++ PesterScriptAnalysis.ps1
@@ -0,0 +1,65 @@
+Param (
+[String]$Path,
+[String]$CustomRulePath
+)
+
+Describe 'Testing PSSA Standard rule set' {
+    $AnalysisResults = Invoke-ScriptAnalyzer -Path $Path
+
+    Context 'Rules with a severity rating of "Error"' {
+        $Rules = Get-ScriptAnalyzerRule -Severity Error
+        Foreach($Rule in $Rules)
+        {
+            It "$($Rule.RuleName)" {
+                If($AnalysisResults.RuleName -contains $Rule.RuleName)
+                {
+                    $Failed = $true
+                }
+                Else
+                {
+                    $Failed = $false
+                }
+                $Failed | Should -Be $false
+            }
+        }
+    }
+    Context 'Rules with a severity rating of "Warning"' {
+        $Rules = Get-ScriptAnalyzerRule -Severity Warning
+        Foreach($Rule in $Rules)
+        {
+            It "$($Rule.RuleName)" {
+                If($AnalysisResults.RuleName -contains $Rule.RuleName)
+                {
+                    $Failed = $true
+                }
+                Else
+                {
+                    $Failed = $false
+                }
+                
+                $Failed | Should -Be $false
+            }
+        }
+    }
+}
+If(Test-Path $CustomRulePath) {
+    Describe 'Testing PSSA Custom rule set' {
+        $CustomRUleAnalysisResults = Invoke-ScriptAnalyzer -Path $Path -CustomRulePath $CustomRulePath
+        $Rules = Get-ScriptAnalyzerRule -CustomRulePath $CustomRulePath
+        Foreach($Rule in $Rules)
+        {
+            It "$($Rule.RuleName)" {
+                If($AnalysisResults.RuleName -contains $Rule.RuleName)
+                {
+                    $Failed = $true
+                }
+                Else
+                {
+                    $Failed = $false
+                }
+                
+                $Failed | Should -Be $false
+            }
+        }
+    }
+} 
