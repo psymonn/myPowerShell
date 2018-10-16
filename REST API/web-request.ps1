927e6fed657a9f90d7a65ab66697ed52e96a3d77
psymonn <40018954+psymonn@users.noreply.github.com>
2018-10-16 16:15:33 +1100

    Create web-request.ps1

---
+++ REST API/web-request.ps1
@@ -0,0 +1,22 @@
+# Create Header
+             $AuthInfo = "[Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f 'admin', 'pass')))"
+
+            $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+            #$headers.Add('Authorization',('Basic {0}' -f $AuthInfo))
+            $headers.Add('Accept','application/json')
+            $headers.Add('Content-Type','application/json')
+
+                        
+           # $Body = @{$number = '31'} | ConvertTo-Json
+            
+
+             $RestSplat = @{
+                    Headers = $headers
+                    Method = "post"
+                    Uri = "http://localhost:8080/job/Test1/api/json?pretty=true"
+                    #Body = $Body
+                }
+
+
+
+                Invoke-WebRequest @RestSplat -ErrorAction Stop
